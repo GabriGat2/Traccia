08c1caa2764727d085990640e442a9180b1c694a
GabriGat2 <gabri.naba21a@gmail.com>
2025-09-16 09:29:58 +0200

    Selezione della traccia da modificare dal form sommario 2

--- Traccia/FormArchivoTraccia.cs
+++ Traccia/FormArchivoTraccia.cs
@@ -686,20 +686,16 @@ namespace Traccia
         /// <returns></returns>
         private GstErrori.EErrore ModificaTraccia()
         {
-            // Seleziona una traccia
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.InitialDirectory = Traccia.GetPathInfo();
-            dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            dlg.FilterIndex = 1;
-            dlg.RestoreDirectory = true;
-
-            if (dlg.ShowDialog() != DialogResult.OK)
-            {
+            /// apre il sommario delle tracce per selezionare una traccia
+            FormSommario dlg = new FormSommario(ref Traccia);
+            dlg.ShowDialog();
+            if (dlg.DialogResult != DialogResult.OK)
                 return GstErrori.EErrore.E0001_NOK;
-            }
+            else if (dlg.PathTracciaSelezionata == null)
+                return GstErrori.EErrore.E0001_NOK;
 
             // Estrae il nome del file info
-            string pathFileInfo = dlg.FileName;
+            string pathFileInfo = dlg.PathTracciaSelezionata;
 
             // Legge il file info della traccia
             GstErrori.EErrore esito = Traccia.LeggeFileInfo(pathFileInfo);
